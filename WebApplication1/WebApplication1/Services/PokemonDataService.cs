ee2014972131e50c06f7f4f30072b261fd9c2cc3
Rakko Raj H. Delos Santos <1264-22@itmlyceumalabang.onmicrosoft.com>
2025-04-01 20:06:15 +0800

    Update PokemonDataService.cs

--- WebApplication1/WebApplication1/Services/PokemonDataService.cs
+++ WebApplication1/WebApplication1/Services/PokemonDataService.cs
@@ -106,6 +106,74 @@ namespace WebApplication1.Services
                     ImageUrl = "https://img.pokemondb.net/artwork/meganium.jpg",
                     Height = 18, Weight = 1005
                 },
+                 new Pokemon {
+                    Id = 25, Name = "Pikachu", Types = new List<string> { "Electric" },
+                    Generation = 1, NextEvolutionId = 26, BaseEvolutionId = 172,
+                    ImageUrl = "https://img.pokemondb.net/artwork/pikachu.jpg",
+                    Height = 4, Weight = 60
+                 },
+                new Pokemon {
+                    Id = 26, Name = "Raichu", Types = new List<string> { "Electric" },
+                    Generation = 1, NextEvolutionId = null, BaseEvolutionId = 25,
+                    ImageUrl = "https://img.pokemondb.net/artwork/raichu.jpg",
+                    Height = 8, Weight = 300
+                },
+
+                // Generation 1 - Geodude Line
+                new Pokemon {
+                    Id = 74, Name = "Geodude", Types = new List<string> { "Rock", "Ground" },
+                    Generation = 1, NextEvolutionId = 75, BaseEvolutionId = null,
+                    ImageUrl = "https://img.pokemondb.net/artwork/geodude.jpg",
+                    Height = 4, Weight = 200
+                },
+                new Pokemon {
+                    Id = 75, Name = "Graveler", Types = new List<string> { "Rock", "Ground" },
+                    Generation = 1, NextEvolutionId = 76, BaseEvolutionId = 74,
+                    ImageUrl = "https://img.pokemondb.net/artwork/graveler.jpg",
+                    Height = 10, Weight = 1050
+                },
+        
+                // Generation 1 - Abra Line
+                new Pokemon {
+                    Id = 63, Name = "Abra", Types = new List<string> { "Psychic" },
+                    Generation = 1, NextEvolutionId = 64, BaseEvolutionId = null,
+                    ImageUrl = "https://img.pokemondb.net/artwork/abra.jpg",
+                    Height = 9, Weight = 195
+                },
+                new Pokemon {
+                    Id = 64, Name = "Kadabra", Types = new List<string> { "Psychic" },
+                    Generation = 1, NextEvolutionId = 65, BaseEvolutionId = 63,
+                    ImageUrl = "https://img.pokemondb.net/artwork/kadabra.jpg",
+                    Height = 13, Weight = 565
+                },
+        
+                // Generation 2 - Cyndaquil Line
+                new Pokemon {
+                    Id = 155, Name = "Cyndaquil", Types = new List<string> { "Fire" },
+                    Generation = 2, NextEvolutionId = 156, BaseEvolutionId = null,
+                    ImageUrl = "https://img.pokemondb.net/artwork/cyndaquil.jpg",
+                    Height = 5, Weight = 79
+                },
+                new Pokemon {
+                    Id = 156, Name = "Quilava", Types = new List<string> { "Fire" },
+                    Generation = 2, NextEvolutionId = 157, BaseEvolutionId = 155,
+                    ImageUrl = "https://img.pokemondb.net/artwork/quilava.jpg",
+                    Height = 9, Weight = 190
+                },
+        
+                // Generation 2 - Totodile Line
+                new Pokemon {
+                    Id = 158, Name = "Totodile", Types = new List<string> { "Water" },
+                    Generation = 2, NextEvolutionId = 159, BaseEvolutionId = null,
+                    ImageUrl = "https://img.pokemondb.net/artwork/totodile.jpg",
+                    Height = 6, Weight = 95
+                },
+                new Pokemon {
+                    Id = 159, Name = "Croconaw", Types = new List<string> { "Water" },
+                    Generation = 2, NextEvolutionId = 160, BaseEvolutionId = 158,
+                    ImageUrl = "https://img.pokemondb.net/artwork/croconaw.jpg",
+                    Height = 11, Weight = 250
+                }
 
             };
         }
